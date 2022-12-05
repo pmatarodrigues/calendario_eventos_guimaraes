8a5a676aee15326647b0a94c6d6c10702653a2a3
mata <didiandrevsc@gmail.com>
2022-12-05 01:30:20 +0000

    start project

---
+++ cultural_events.py
@@ -0,0 +1,27 @@
+
+url = "http://em.guimaraes.pt/agenda"
+
+html_doc = getHTMLdoc(url)
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+titulos = soup.findAll('h2')
+
+eventos = {}
+# print(soup.select(".cell .linl_block .linl_inner"))
+for event in soup.select('.cell .linl_block .linl_inner'):
+    # print(event)
+
+    event_data = parse_event(event)
+
+    titulo = event.find('h2').getText()
+    # print(event.select_one(".summary").p)
+    eventos[titulo] = {
+        "titulo": titulo,
+        # "subtitulo": event.find('.summary p'),
+        # "data": ,
+        # "local": ,
+        # "tema":
+    }
+
+# print(eventos)
